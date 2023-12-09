1ce8ad0546fabb6e51a6b06db932200406a9e088
GaryVT <gvilcat@ulasalle.edu.pe>
2023-12-09 12:35:21 -0500

    Chat implementado sin bd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,15 @@ import HomePage from "./pages/HomePage";
 import SignInPage from "./pages/SignInPage";
 import LoginGoogle from "./pages/LoginGoogle";
 import Perfil from "./pages/Perfil";
+import io from "socket.io-client";
 
-// const socket = io("/");
+
+// const socket = io("http://localhost:3000");
+
+//backend 4000 - 3000
+//frontend 5173
+
+ const socket = io("http://localhost:5173");
 
 const route = createBrowserRouter([
   {
--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,10 +1,44 @@
 import React, { useState, useRef, useEffect } from "react";
 import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import SendIcon from '@mui/icons-material/Send';
 import IconButton from "@mui/material/IconButton";
-import { Close as CloseIcon, ThumbUpAltOutlined } from "@mui/icons-material";
+import { Close as CloseIcon } from "@mui/icons-material";
 import io from "socket.io-client";
 
-import MessageInput from "./MessageInput"; // Asegúrate de tener tu componente de MessageInput creado
+const socket = io("/");
+
+const MessageInput = ({ message, setMessage, sendMessage }) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(message);
+    socket.emit('message', message);
+    sendMessage();
+  };
+
+  useEffect(() => {
+    socket.on('message', message => {
+      console.log(message);
+    });
+  }, []);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Box display="flex" alignItems="center" gap={2}>
+        <TextField
+          fullWidth
+          label="Escribe tu mensaje"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <Button color="secondary" variant="contained" type="submit">
+          <SendIcon />
+        </Button>
+      </Box>
+    </form>
+  );
+};
 
 const Chat = ({ onClose, appBarBackgroundColor }) => {
   const [messages, setMessages] = useState([]);
@@ -12,11 +46,9 @@ const Chat = ({ onClose, appBarBackgroundColor }) => {
   const messagesEndRef = useRef(null);
   const socketRef = useRef();
 
-
   useEffect(() => {
-    socketRef.current = io("http://localhost:3000"); // Reemplaza con la URL de tu servidor Socket.IO
-
-    socketRef.current.on("newMessage", (newMessage) => {
+    socketRef.current = io("/");
+    socketRef.current.on("message", (newMessage) => {
       setMessages((prevMessages) => [...prevMessages, newMessage]);
       scrollToBottom();
     });
@@ -39,45 +71,6 @@ const Chat = ({ onClose, appBarBackgroundColor }) => {
       setMessages((prevMessages) => [...prevMessages, newMessage]);
       setMessage('');
     }
-  };
-
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter' && !event.shiftKey) {
-      event.preventDefault();
-      sendMessage();
-    }
-  };
-
-/*
-  const sendMessage = () => {
-    if (message.trim() !== '') {
-      setMessages(prevMessages => [
-        ...prevMessages,
-        { text: message, user: 'Usuario' }
-      ]);
-      setMessage('');
-    }
-  };
-
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter' && !event.shiftKey) {
-      event.preventDefault();
-      sendMessage();
-    }
-  };
-  */
-
-  const chatStyle = {
-    border: `2px solid`,
-    borderRadius: '10px',
-    position: 'fixed',
-    bottom: '3px',
-    right: '300px',
-    zIndex: 999,
-    backgroundColor: appBarBackgroundColor,
-    height: '400px',
-    display: 'flex',
-    flexDirection: 'column',
   };
 
   const headerStyle = {
@@ -116,7 +109,18 @@ const Chat = ({ onClose, appBarBackgroundColor }) => {
     maxWidth: '70%',
   };
 
-
+  const chatStyle = {
+    border: `2px solid`,
+    borderRadius: '10px',
+    position: 'fixed',
+    bottom: '3px',
+    right: '300px',
+    zIndex: 999,
+    backgroundColor: appBarBackgroundColor,
+    height: '400px',
+    display: 'flex',
+    flexDirection: 'column',
+  };
 
   return (
     <Box className="chat-container" style={chatStyle}>
@@ -127,21 +131,19 @@ const Chat = ({ onClose, appBarBackgroundColor }) => {
         </IconButton>
       </Box>
       <Box className="chat-messages" style={chatContentStyle}>
-        {messages.map((msg, index) => (
-          <div key={index} className="message" style={messageStyle}>
-            <span>{msg.user}: </span>
-            <p>{msg.text}</p>
-          </div>
-        ))}
-        <div ref={messagesEndRef} />
-      </Box>
+  {messages.map((msg, index) => (
+    <div key={index} className="message" style={messageStyle}>
+      {msg.text && <span>{msg.user}</span>}
+      {msg.text && <p>{msg.text}</p>}
+    </div>
+  ))}
+  <div ref={messagesEndRef} />
+</Box>
       <Box className="message-input" style={messageInputContainerStyle}>
         <MessageInput
           message={message}
           setMessage={setMessage}
           sendMessage={sendMessage}
-          onKeyDown={handleKeyDown}
-          style={{ flex: 1 }}
         />
       </Box>
     </Box>
@@ -149,3 +151,5 @@ const Chat = ({ onClose, appBarBackgroundColor }) => {
 };
 
 export default Chat;
+
+
